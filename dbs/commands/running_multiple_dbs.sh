dd2019087fef6d3b22055ca87a2b47df82e1c93f
lmiadowicz <l.miadowicz@gmail.com>
2021-02-08 22:43:49 +0100

    added multiple db handling queries

--- dbs/commands/running_multiple_dbs.sh
+++ dbs/commands/running_multiple_dbs.sh
@@ -1,3 +1,32 @@
+#Running multiple PostgreSQL versions simultaneously on Mac OS or Linux
+
 #stop postgres on Mac
 pg_ctl stop -W -t 1 -s -D /usr/local/var/postgres -m f
 
+#MAC - installing multiple versions of Postgresql 
+brew tap petere/postgresql
+brew install postgresql-12
+brew install postgresql-11
+brew uninstall postgresql
+brew install postgresql-common
+
+#pg_createcluster [options] <version> <name>
+pg_createcluster 9.3 main
+pg_lsclusters
+#start/stop/restart/reload clusters
+pg_ctlcluster 9.6 main start
+#delete cluster
+pg_dropcluster <version> <name>
+psql --cluster 9.3/main -p port
+
+
+#LINUX - installing multiple versions of Postgresql
+sudo apt install postgresql-9.5
+
+sudo systemctl start postgresql@9.5-main (stop to stop)
+sudo systemctl start postgresql@10-main
+
+#find all clusters
+pg_lsclusters
+
+#version config /etc/postgresql/postgresql.conf
